Fixed GetGradients to watch Noise and calculate gradients for noise

adversary/attacks/whitebox/wb_targeted_attack.py
@@ -5,6 +5,7 @@ from adversary.core.optimizers.optimizer_base import Optimizer
 from tensorflow.keras.models import Model as TFModel
 import tensorflow as tf
 from . import WhiteBoxAttack
+import torch
 
 import numpy as np
 
@@ -21,8 +22,8 @@ class TargetedWhiteBoxAttack(WhiteBoxAttack):
         super().__init__(model, loss, optimizer, optimizer_params, noise_generator, *args, **kwargs)
 
     def attack(self,
-               sample,
-               target_class,
+               sample: Union[np.ndarray, torch.Tensor, tf.Tensor],
+               target_class: int,
                target_vector=None,
                epochs=10,
                *args,
@@ -39,9 +40,11 @@ class TargetedWhiteBoxAttack(WhiteBoxAttack):
         if len(target_vector) != self.model_info["output_shape"][1]:
             raise ValueError("target_vector must be the same size outputs.")
         
-        noise = self.noise_generator.generate()
+        noise = self.noise_generator.generate(sample)
+
+        gradeints = self.get_grads.calculate(self.model, sample, noise, self.noise_generator, target_vector)
         
-        return target_vector
+        return gradeints
         
 
 

adversary/core/get_grad/getgrad.py
@@ -1,4 +1,6 @@
 from . import GetGradsBase
+from adversary.core.noise_generators import NoiseGenerator
+from adversary.core.losses import Loss
 
 from typing import Literal, List, Union
 
@@ -8,38 +10,45 @@ import torch
 import tensorflow as tf
 
 
-
 class GetGrads(GetGradsBase):
-    def __init__(self, 
+    def __init__(self,
                  framework: Literal["torch", "tf"],
-                 loss) -> None:
+                 loss: Loss) -> None:
         super().__init__(None, loss)
         self.framework = framework
         self.loss = loss
 
-    def calculate(self, 
-                  model: Union[torch.nn.Module, tf.keras.Model], 
-                  inputs: Union[torch.Tensor, tf.Tensor], 
+    def calculate(self,
+                  model: Union[torch.nn.Module, tf.keras.Model],
+                  sample: Union[torch.Tensor, tf.Tensor],
+                  noise: Union[torch.Tensor, tf.Tensor],
+                  noise_generator: NoiseGenerator,
                   targets: Union[torch.Tensor, tf.Tensor]):
-        return super().calculate(model, inputs, targets)
+        return super().calculate(model, sample, noise, noise_generator, targets)
 
-    def torch_op(self, 
-                 model: torch.nn.Module, 
-                 inputs: torch.Tensor, 
-                 targets: torch.Tensor
-                 ) -> List[torch.Tensor]:
-        model.zero_grad()
-        outputs = model(inputs)
-        loss = self.loss.calculate(outputs, targets)
-        loss.backward()
-        return [param.grad for param in model.parameters() if param.grad is not None]
+    def torch_op(self,
+                 model: Union[torch.nn.Module, tf.keras.Model],
+                 sample: Union[torch.Tensor, tf.Tensor],
+                 noise: Union[torch.Tensor, tf.Tensor],
+                 noise_generator: NoiseGenerator,
+                 targets: Union[torch.Tensor, tf.Tensor]) -> List[torch.Tensor]:
+        raise NotImplementedError("Not implemented for Torch")
 
-    def tf_op(self, 
-              model: tf.keras.Model, 
-              inputs: tf.Tensor, 
+    def tf_op(self,
+              model: tf.keras.Model,
+              sample: tf.Tensor,
+              noise: tf.Tensor,
+              noise_generator: NoiseGenerator,
               targets: tf.Tensor
               ) -> List[tf.Tensor]:
+        noise = tf.Variable(noise, trainable=True)
+
         with tf.GradientTape() as tape:
-            outputs = model(inputs)
+            tape.watch(noise)
+            
+            input = noise_generator.apply_noise(sample, noise)
+            outputs = model(input)
             loss = self.loss.calculate(outputs, targets)
-        return tape.gradient(loss, model.trainable_variables)
+  
+        gradients = tape.gradient(loss, noise)
+        return gradients

adversary/core/noise_generators/additive_noise_generator.py
@@ -5,6 +5,8 @@ import torch
 import tensorflow as tf
 from torch.nn import functional as F
 
+import numpy as np
+
 class AdditiveNoiseGeneratorTF(NoiseGenerator, metaclass=NoiseGeneratorMeta):
     def __init__(self,
                  framework: Literal["torch", "tf"],
@@ -22,7 +24,15 @@ class AdditiveNoiseGeneratorTF(NoiseGenerator, metaclass=NoiseGeneratorMeta):
         self.scale = scale
 
     def generate(self, 
-              shape: List[int]) -> tf.Tensor:
+                 sample: Union[np.ndarray, torch.Tensor, tf.Tensor]) -> tf.Tensor:
+
+        if not isinstance(sample, (np.ndarray, torch.Tensor, tf.Tensor)):
+            raise TypeError("Input must be of type np.ndarray, torch.Tensor, or tf.Tensor")
+
+        shape = sample.shape
+        if shape[0] is None:
+            shape = shape[1:]
+        
         if self.dist == "normal":
             noise = tf.random.normal(shape)
             noise = tf.clip_by_value(noise, -1, 1) * self.epsilon
@@ -36,6 +46,7 @@ class AdditiveNoiseGeneratorTF(NoiseGenerator, metaclass=NoiseGeneratorMeta):
             target_min, target_max = self.scale
             noise = (noise - current_min) / (current_max - current_min) * (target_max - target_min) + target_min
         
+        noise = tf.Variable(noise)
         return noise
     
     def apply_noise(self, sample: tf.Tensor, noise: tf.Tensor) -> tf.Tensor: