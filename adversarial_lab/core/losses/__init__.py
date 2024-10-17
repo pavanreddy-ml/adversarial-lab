cea24b410ec17bfe405a07ba2a597f62775839e7
MadhavNair4 <ranjitnayar8888@gmail.com>
2024-10-18 03:00:41 +0530

    Implement Mean Absolute Error

--- adversarial_lab/core/losses/__init__.py
+++ adversarial_lab/core/losses/__init__.py
@@ -1,7 +1,8 @@
 from .loss_base import Loss
 from .categorical_cross_entropy import CategoricalCrossEntropy
 from .binary_cross_entropy import BinaryCrossEntropy
+from .mean_absolute_error import MeanAbsoluteError
 from .loss_registry import LossRegistry
 
 
-__all__ = ["Loss", "CategoricalCrossEntropy", "BinaryCrossEntropy", "LossRegistry"]
+__all__ = ["Loss", "CategoricalCrossEntropy", "BinaryCrossEntropy", "MeanAbsoluteError", "LossRegistry"]
