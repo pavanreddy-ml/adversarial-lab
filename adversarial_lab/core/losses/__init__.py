ccafdb573cc1fe50f4baa6bd9a3f6ff5702a2f16
MadhavNair4 <ranjitnayar8888@gmail.com>
2024-10-20 02:20:57 +0530

    Implement MSE Loss

--- adversarial_lab/core/losses/__init__.py
+++ adversarial_lab/core/losses/__init__.py
@@ -2,7 +2,9 @@ from .loss_base import Loss
 from .categorical_cross_entropy import CategoricalCrossEntropy
 from .binary_cross_entropy import BinaryCrossEntropy
 from .mean_absolute_error import MeanAbsoluteError
+from .mean_squared_error import MeanSquaredError
 from .loss_registry import LossRegistry
 
 
-__all__ = ["Loss", "CategoricalCrossEntropy", "BinaryCrossEntropy", "MeanAbsoluteError", "LossRegistry"]
+__all__ = ["Loss", "CategoricalCrossEntropy", "BinaryCrossEntropy", "MeanAbsoluteError", 
+           "MeanSquaredError", "LossRegistry"]
